769365138690011c5eab588ae7455f0e573f9f83
TonyTheryx <140670936+TonyTheryx@users.noreply.github.com>
2026-04-10 23:19:05 -0600

    Eliminar StartScreen

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import "./global.css";
 import React, { useState } from "react";
 import LoginScreen from "./src/screens/Principal/LoginScreen";
 import RegisterScreen from "./src/screens/Principal/RegisterScreen";
-import StartScreen from "./src/screens/Principal/StartScreen";
+import HomeScreen from "./src/screens/Principal/HomeScreen";
 import ForgetPasswordScreen from "./src/screens/Principal/ForgetPasswordScreen";
 import CodeForgetPasswordScreen from "./src/screens/Principal/CodeForgetPasswordScreen";
 import ConfPerfilScreen from "./src/screens/Principal/ConfPerfilScreen";
@@ -11,7 +11,7 @@ import ChangePasswordScreen from "./src/screens/Principal/ChangePasswordScreen";
 type ScreenName =
   | "Login"
   | "Register"
-  | "Start"
+  | "Home"
   | "Forget"
   | "Code"
   | "ConfigP"
@@ -29,7 +29,7 @@ export default function App() {
       }
       if (name === "Register") setScreen("Register");
       if (name === "Login") setScreen("Login");
-      if (name === "Start") setScreen("Start");
+      if (name === "Home") setScreen("Home");
       if (name === "Forget") setScreen("Forget");
       if (name === "Code") setScreen("Code");
       if (name === "ConfigP") setScreen("ConfigP");
@@ -37,8 +37,8 @@ export default function App() {
     },
   } as any;
 
-  return screen === "Start" ? (
-    <StartScreen navigation={navigation} />
+  return screen === "Home" ? (
+    <HomeScreen navigation={navigation} />
   ) : screen === "Register" ? (
     <RegisterScreen navigation={navigation} />
   ) : screen === "Forget" ? (
