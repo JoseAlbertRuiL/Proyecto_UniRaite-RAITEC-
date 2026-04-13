2088b4eb0bce5dff8dcf5567d020618e76414e25
Giovani_dev <giojuarezz174@gmail.com>
2026-04-13 14:40:16 -0600

    Osiosi

--- backend/prisma/migrations/20260410164502_init/migration.sql
+++ backend/prisma/migrations/20260410164502_init/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "EstadoSolicitud" AS ENUM ('pendiente', 'aceptada', 'rechazada');
 
@@ -12,9 +6,6 @@ CREATE TYPE "EstadoTrayecto" AS ENUM ('en_curso', 'finalizado', 'cancelado');
 
 -- CreateEnum
 CREATE TYPE "TipoEmergencia" AS ENUM ('accidente', 'acoso', 'falla_mecanica', 'otro');
-
--- DropTable
-DROP TABLE "User";
 
 -- CreateTable
 CREATE TABLE "usuarios" (
