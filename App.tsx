aa3a9371f02d11e5083ec1f79a463b20c73e9684
BredFan146 <sebastianda971@gmail.com>
2026-04-17 21:34:08 -0600

    feat: agrega interfaz visual para calificar viaje

--- App.tsx
+++ App.tsx
@@ -3,17 +3,19 @@ import React, { useState } from 'react';
 import LoginScreen from './src/screens/Principal/LoginScreen';
 import RegisterScreen from './src/screens/Principal/RegisterScreen';
 import StartScreen from "./src/screens/Principal/HomeScreen";
+import RateTripScreen from './src/screens/trip/RateTripScreen';
 
-type ScreenName = 'Login' | 'Register' | 'Start';
+type ScreenName = 'Login' | 'Register' | 'Start' | 'RateTrip';
 
 export default function App() {
   const [screen, setScreen] = useState<ScreenName>('Start');
 
   const navigation = {
-    navigate: (name: string) => {
+    navigate: (name: ScreenName) => {
       if (name === 'Register') setScreen('Register');
       if (name === 'Login') setScreen('Login');
       if (name === 'Start') setScreen('Start');
+      if (name === 'RateTrip') setScreen('RateTrip');
     },
   } as any;
 
@@ -21,6 +23,8 @@ export default function App() {
     <LoginScreen navigation={navigation} />
   ) : screen === 'Register' ? (
     <RegisterScreen navigation={navigation} />
+  ) : screen === 'RateTrip' ? (
+    <RateTripScreen />
   ) : (
     <StartScreen navigation={navigation} />
   );
